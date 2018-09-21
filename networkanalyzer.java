3a9a9b815b92a1c16e6ec0bf67e1b3590c8e5aff
xiangweizheng <43075724+xiangweizheng@users.noreply.github.com>
2018-09-21 15:18:51 -0700

    add graph generate function and print path
    add graph generate function and print path

--- networkanalyzer.java
+++ networkanalyzer.java
@@ -1,10 +1,20 @@
-package cse360teamproject;
+package cse360project;
 
-public class networkanalyzer {
+
+//package cse360teamproject;
+import java.util.*;
+import cse360project.wnetdiag.Graph; 
+//import cse360teamproject.wnetdiag.Graph; 
+//import cse360teamproject.networkanalyzer.Task;
+
+
+public class netaz {
+//public class networkanalyzer {
 
 	public static void main(String[] args) {
-	    //The example dependency graph from
+	    //Create an HashSet to store tasks
 		HashSet<Task> allTasks = new HashSet<Task>();
+		//demo example
 		int i=0;//task id
 	    Task end = new Task("End",i++, 0);    
 	    Task E = new Task("E",i++,20,end);   
@@ -26,16 +36,61 @@ public class networkanalyzer {
 	    allTasks.add(F);
 	    allTasks.add(H);
 	    allTasks.add(start);//    
-
 	    
-	    //Create an Array List 
-	    HashMap<Integer, String> hmap = new HashMap<Integer, String>();
-
-	    
-	    
-
+	    //Create an hashmap to store pair between task id and name  
+	    HashMap<Integer, String> hmap = new HashMap<Integer, String>(); 	   
 	    System.out.println(allTasks);
-	
+	  
+	    
+	    
+	    
+	    
+	    
+	    //process to generate graph from task
+	    HashSet<Task> remaining = new HashSet<Task>(allTasks);//load tasks into remaining
+	    Graph graph = new Graph(allTasks.size());
+
+	    while(!remaining.isEmpty()){
+
+	      //find a new task to add into graph
+	      for(Iterator<Task> it = remaining.iterator();it.hasNext();){
+	        Task task = it.next();
+	        hmap.put(task.id,task.name);//store task id and name into hashmap
+	        for(Task t : task.dependencies){
+	        	graph.addEgde(task.id, t.id, task.cost);
+	        	//System.out.println(t);
+	          }
+        
+	        it.remove();
+	      }   
+	        
+	    }
+	    //generate path from source to des// need detect source and des
+	    ArrayList<ArrayList<Integer>> allpath = graph.allpath(9,0);
+	    //System.out.println(allpath);
+	    //
+	    Collections.sort(allpath, new Comparator<ArrayList<Integer>>() {    
+	        @Override
+	        public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
+	            return o2.get(0).compareTo(o1.get(0));
+	        }               
+	});
+	   // System.out.println(allpath);//test path print
+	    //System.out.println(allpath.get(0));//test path print
+	    
+	    
+	    //print path of task name 
+	    for(int k=0;k<allpath.size();k++) {
+	    	ArrayList<Integer> tpal=allpath.get(k);
+	    	System.out.println("path cost is "+tpal.get(0)+" path sequence is:");
+	    	for(int m=1;m<tpal.size();m++)
+	    	          {System.out.print(hmap.get(tpal.get(m)));
+	    	          System.out.print(" ");}
+	    	 System.out.print("\n");
+	    	
+	    }
+	   
+	    
 	
 }
 	
