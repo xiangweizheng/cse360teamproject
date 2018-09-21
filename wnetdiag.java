01bf9884716fd8a00fe51265c4a90e2e5473ae73
xiangweizheng <43075724+xiangweizheng@users.noreply.github.com>
2018-09-21 15:48:47 -0700

    add graph to help generate paths

---
+++ wnetdiag.java
@@ -0,0 +1,35 @@
+//package cse360teamproject;
+package cse360project;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.*;
+public class wnetdiag {
+    static class Edge {
+        int source;
+        int destination;
+        int weight;
+
+        public Edge(int source, int destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    static class Graph {
+        int vertices;
+        LinkedList<Edge> [] adjacencylist;
+
+        Graph(int vertices) {
+            this.vertices = vertices;
+            adjacencylist = new LinkedList[vertices];
+            //initialize adjacency lists for all the vertices
+            for (int i = 0; i <vertices ; i++) {
+                adjacencylist[i] = new LinkedList<>();
+            }
+        }
+
+       
+      
+}
